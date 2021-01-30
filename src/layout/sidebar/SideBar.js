4cd779495f8137984f17ad75c8b99d9d03a84bd0
Nguyen Van Huyen <phillipng.working@gmail.com>
2021-01-30 23:27:05 +0700

    layout n setup redux

---
+++ src/layout/sidebar/SideBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SideBar.css'
+
+export const SideBar = () => {
+    return (
+        <aside className="Sidebar">
+            SideBar works.
+        </aside>
+    )
+}
+
